ff1c4d7c02aa72ad1651a235b0a1e1a175bc6d75
lzx <168624142+lzxuandev@users.noreply.github.com>
2026-06-13 18:51:03 +0800

    Update nvim-tree configuration with new options

--- .config/nvim/lua/plugin/nvim-tree.lua
+++ .config/nvim/lua/plugin/nvim-tree.lua
@@ -2,34 +2,41 @@ vim.pack.add({"https://github.com/nvim-tree/nvim-tree.lua"})
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-
 require("nvim-tree").setup({
+
+    hijack_cursor = true,
+
     sort = {
-        sorter = "case_sensitive", 
+        sorter = "case_sensitive",
     },
 
     view = {
-        width = 30,             
+        width = 30,
         side = "left",
-        cursorline = true,  
+        cursorline = true,
     },
 
     renderer = {
-        group_empty = true,     
-        root_folder_label = ":t",  
+        group_empty = true,
+        root_folder_label = ":t",
     },
 
     filters = {
-        dotfiles = true,   
+        dotfiles = true,
     },
 
     actions = {
         open_file = {
             quit_on_open = true,
         }
-    }
+    },
+
+    update_focused_file = {
+        enable = true,
+        update_root = {
+            enable = true,
+            ignore_list = {},
+        }
+    },
 
 })
-
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { silent = true, desc = 'Toggle NvimTree' })
-
